05334fbb02f8dc3c5836327f0b91a9e735482027
Tenzl <duytien21204@gmail.com>
2026-06-01 11:19:59 +0700

    fix error

--- src/features/notification/notification.module.ts
+++ src/features/notification/notification.module.ts
@@ -4,9 +4,10 @@ import { Notification } from './entities/notification.entity';
 import { NotificationService } from './notification.service';
 import { NotificationController } from './notification.controller';
 import { NotificationSchemaBootstrap } from './notification.schema-bootstrap';
+import { User } from '../auth/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Notification])],
+  imports: [TypeOrmModule.forFeature([Notification, User])],
   providers: [NotificationService, NotificationSchemaBootstrap],
   controllers: [NotificationController],
   exports: [NotificationService],
